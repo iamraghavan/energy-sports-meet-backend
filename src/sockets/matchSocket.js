54a75323a688640ea834919d47a6b48750b5077a
iamraghavan <developer.raghavan@gmail.com>
2026-02-20 23:11:38 +0530

    01010

--- src/sockets/matchSocket.js
+++ src/sockets/matchSocket.js
@@ -1,25 +1,26 @@
 const { Match } = require('../models');
+const logger = require('../utils/logger');
 
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        console.log('New client connected:', socket.id);
+        logger.info(`🔌 New client connected: ${socket.id}`);
 
         // 1. Join Specific Match Room (Detailed View)
         socket.on('join_match', (matchId) => {
             socket.join(matchId);
-            console.log(`Socket ${socket.id} joined match room: ${matchId}`);
+            logger.info(`🏠 Socket ${socket.id} joined match room: ${matchId}`);
         });
 
         // 2. Join Overview Room (Dashboard View)
         // Client should emit 'join_overview' when on the main list page
         socket.on('join_overview', () => {
             socket.join('live_overview');
-            console.log(`Socket ${socket.id} joined live_overview`);
+            logger.info(`📊 Socket ${socket.id} joined live_overview`);
         });
 
         socket.on('leave_match', (matchId) => {
             socket.leave(matchId);
-            console.log(`Socket ${socket.id} left match room: ${matchId}`);
+            logger.info(`🚪 Socket ${socket.id} left match room: ${matchId}`);
         });
 
         socket.on('leave_overview', () => {
@@ -27,7 +28,7 @@ module.exports = (io) => {
         });
 
         socket.on('disconnect', () => {
-            console.log('Client disconnected:', socket.id);
+            logger.info(`🔌 Client disconnected: ${socket.id}`);
         });
     });
 };
